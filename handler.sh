fd82ba927a3dbcd5d77380ac3744efdcbad5834f
Eduardo Banderas <ebanderas@alfredsmart.com>
2025-02-05 23:59:55 +0100

    fix logging

--- handler.sh
+++ handler.sh
@@ -3,6 +3,25 @@
 # Event handler to execute backups for the server
 #
 
+_DEBUG=false
+_QUIET=true
+_LOGGING=true
+
+# keep last backups
+_DEFAULT_DAYS=15
+
+_RCLONE_KEEP_BACKUP_DAYS=${_DEFAULT_DAYS}
+_POSTGRESQL_KEEP_BACKUP_DAYS=${_DEFAULT_DAYS}
+_MYSQL_KEEP_BACKUP_DAYS=${_DEFAULT_DAYS}
+_VBOX_KEEP_BACKUP_DAYS=${_DEFAULT_DAYS}
+
+# default run backups
+_RUN_BACKUP_RCLONE=false
+_RUN_BACKUP_MYSQL=false
+_RUN_BACKUP_POSTGRESQL=false
+_RUN_BACKUP_VBOX=false
+
+# return code for functions
 __RETURNCODE_OK=0
 __RETURNCODE_NOTHING=1256
 __RETURNCODE_SCRIPT_NOTHING=1255
@@ -12,26 +31,16 @@ __RETURNCODE_SCRIPT_CLEAN_OLD_BACKUPS=1252
 __RETURNCODE_SSH_FAILED=1251
 __RETURNCODE_SSH_FAILED_ARGUMENTS=1250
 
-_RUN_BACKUP_RCLONE=false
-_RUN_BACKUP_MYSQL=false
-_RUN_BACKUP_POSTGRESQL=false
-_RUN_BACKUP_VBOX=false
-
 _NOW_DATE=$(/bin/date +%Y%m%d)
 
-_DEBUG=false
-_QUIET=true
-_LOGGING=true
-
 _ROOT="$(/bin/dirname $(/bin/readlink -f $0))"
+
+# default arguments
 _CONFIG_FILE="${_ROOT}/config/backup.conf"
 _CONFIG_RCLONE="${_ROOT}/config/rclone.conf"
-
 _SCRIPTS_DIR="${_ROOT}/scripts"
 _LOGDIR="${_ROOT}/logs"
-
 _PIDFILE=/var/run/hanled-backup.locked
-_EXEC="null"
 
 . ${_ROOT}/manager-functions
 
@@ -42,7 +51,6 @@ main() {
   load_fileconf ${_CONFIG_FILE}
 
   for script in $(ls ${_SCRIPTS_DIR}); do
-
     . ${_SCRIPTS_DIR}/${script}
 
     run
@@ -67,31 +75,32 @@ main() {
 
 load_fileconf() {
   [ ! -f "${1}" ] && {
-    __logger ${__ERROR} Not found configuration file, ${1}
+    __logger ${__ERROR} "Not found configuration file, ${1}\n"
   }
 
   . $1
 
   [ -z "${RCLONE_BACKUP_REMOTE}" ] && {
-    __logger ${__ERROR} RCLONE_BACKUP_REMOTE not define
+    __logger ${__ERROR} "RCLONE_BACKUP_REMOTE not define\n"
   }
 }  #load_fileconf
 
 
 print_help() {
   cat << EOF
---quiet|-q \tEnable quiet mode, not output in stdout
---logdir| -l \tSet directory for save output in file when debug mode is enable, default ${_LOGDIR}. The name file is the self of script file name
---config-file|-c \tSet backup file, default ${_CONFIG_FILE}
---config-rclone|r \tSet rclone file configuration, default ${_CONFIG_RCLONE}
---pidfile|-p \tSet pidfile file, default ${_PIDFILE}
+--quiet|-q         Enable quiet mode, not output in stdout
+--script-dir|-s    Set directory for custom scripts, default ${_SCRIPTS_DIR}. The name file is the self of script file name
+--log-dir|-l       Set directory for save output in file when debug mode is enable, default ${_LOGDIR}. The name file is the self of script file name
+--config-file|-c   Set backup file, default ${_CONFIG_FILE}
+--config-rclone|r  Set rclone file configuration, default ${_CONFIG_RCLONE}
+--pid-file|-p      Set pidfile file, default ${_PIDFILE}
 EOF
 }  #print_help
 
 
 print_usage() {
   cat << EOF
-Usage: $0 [--help] [--quiet|-q] [--logdir|-l /path/to/dir]  [--config-file|-c /path/to/file] [--config-rclone|- /path/to/file]  [--pidfile|-p /path/to/pidfile]
+Usage: $0 [--help] [--quiet|-q] [--log-dir|-l /path/to/dir]  [--script-dir|-s /path/to/dir]  [--config-file|-c /path/to/file] [--config-rclone|- /path/to/file]  [--pid-file|-p /path/to/pidfile]
 EOF
 }  #print_usage
 
@@ -113,13 +122,16 @@ parse_arguments() {
       --config-rclone|-r)
         _CONFIG_RCLONE=$(/bin/readlink -f "${value}")
         ;;
-      --logdir|-l)
+      --log-dir|-l)
         _LOGDIR=$(/bin/readlink -f "${value}")
+        ;;
+      --script-dir|-s)
+        _SCRIPTS_DIR=$(/bin/readlink -f "${value}")
         ;;
       --quiet|-q)
         _QUIET=false
         ;;
-      --pidfile|-p)
+      --pid-file|-p)
         _PIDFILE=$value
         ;;
       --help)
@@ -138,15 +150,19 @@ parse_arguments() {
   done
 
   [ ! -f ${_CONFIG_FILE} ] && {
-    __logger ${__ERROR} Not found ${_CONFIG_FILE}
+    __logger ${__ERROR} "Not found ${_CONFIG_FILE}\n"
   }
 
   [ ! -f ${_CONFIG_RCLONE} ] && {
-    __logger ${__ERROR} Not found ${_CONFIG_RCLONE}
+    __logger ${__ERROR} "Not found ${_CONFIG_RCLONE}\n"
+  }
+
+  [ ! -d ${_SCRIPTS_DIR} ] && {
+    __logger ${__ERROR} "Not found ${_SCRIPTS_DIR} directory\n"
   }
 
   [ ! -d ${_LOGDIR} ] && {
-    __logger ${__WARN} Not found ${_LOGDIR}, creating logs directory
+    __logger ${__WARN} "Not found ${_LOGDIR}, creating logs directory\n"
 
     mkdir -p "${_LOGDIR}"
   }
